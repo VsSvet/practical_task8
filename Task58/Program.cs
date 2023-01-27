99e5aebe496b804fd08f8cab6993a4a8ed0fff76
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-28 01:52:24 +0300

    add Task58

---
+++ Task58/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[][] matrix1 = new int[][]
+{
+    new int[] { 1, 4, 7, 2 },
+    new int[] { 5, 9, 3, 2 },
+    new int[] { 8, 4, 2, 4 },
+    new int[] { 5, 2, 6, 7 }
+};
+
+int[][] matrix2 = new int[][]
+{
+    new int[] { 1, 5, 8, 5 }, 
+    new int[] { 4, 9, 4, 2 },
+    new int[] { 7, 2, 2, 6 },
+    new int[] { 2, 3, 4, 7 }
+};
+
+WriteLine("Произведение двух матриц: ");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Write($"{matrix1[i][j]*matrix2[i][j]} ");
+    }
+    WriteLine();
+}
+
+
+
+
+
