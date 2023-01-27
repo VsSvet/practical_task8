73543bd5cec9a9ccfcee307653c34074108d9187
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-28 01:52:40 +0300

    add Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int X = 2;
+int Y = 2;
+int Z = 2;
+int[,,] result = new int[X, Y, Z];
+List<int> numbers = new List<int> { 12, 22, 31, 45, 53, 64, 78, 82 };
+
+WriteLine("Заданный массив: ");
+for (int i = 0; i < X; i++)
+{
+    for (int j = 0; j < Y; j++)
+    {
+        for (int k = 0; k < Z; k++)
+        {
+            
+            result[i, j, k] = numbers[0];
+            numbers.RemoveAt(0);
+            Write($"{result[i, j, k]}({i},{j},{k}) ");
+        }
+        WriteLine();
+    }
+    WriteLine();
+}
